e6ea3bf98977abf6a53aa07019fc659ec75c53d5
YaSh PaReek <74831712+yashhpareek@users.noreply.github.com>
2024-10-14 11:53:37 +0530

    Update Program.cs

--- ToDoBack/Program.cs
+++ ToDoBack/Program.cs
@@ -13,6 +13,7 @@ builder.Services.AddTransient<ITodoService, TodoImp>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Logging.ClearProviders();
 
 builder.Services.AddCors(options =>
 {
